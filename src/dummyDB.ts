0727206db3435926617eb7a85294f469c093c746
Anyssa <patelany05@gmail.com>
2023-05-18 10:35:24 -0400

    complete

--- src/dummyDB.ts
+++ src/dummyDB.ts
@@ -5,4 +5,11 @@ export const dummyCartItems: CartItem[] = [
   { id: 2, product: "Bucket", price: 1.0, quantity: 5 },
   { id: 3, product: "Trash Bags", price: 2.0, quantity: 1 },
   { id: 4, product: "Lawn Bags", price: 1.0, quantity: 10 },
+  { id: 5, product: "Apples", price: 0.99, quantity: 10 },
+  { id: 6, product: "Blueberries", price: 4.37, quantity: 8 },
+  { id: 7, product: "Cinnamon", price: 2.99, quantity: 1 },
+  { id: 8, product: "Cheese", price: 1.5, quantity: 2 },
+  { id: 9, product: "Cream", price: 2.69, quantity: 4 },
+  { id: 10, product: "Crisps", price: 12.99, quantity: 5 },
+  { id: 11, product: "Rhubarb", price: 3.89, quantity: 3 },
 ];
--- src/routes/cartRouter.ts
+++ src/routes/cartRouter.ts
@@ -3,44 +3,40 @@ import { dummyCartItems } from "../dummyDB";
 import { CartItem } from "../models/CartItem";
 
 const cartRouter = express.Router();
-let nextId: number = 6;
+let nextId: number = dummyCartItems.length + 1;
 
-// build endpoints here
 cartRouter.get("/cart-items", (req, res) => {
   const { maxPrice, prefix, pageSize } = req.query;
+  let temporaryCart: CartItem[] = dummyCartItems;
   if (maxPrice) {
-    const filteredArray: CartItem[] = dummyCartItems.filter((item) => {
+    temporaryCart = temporaryCart.filter((item) => {
       return item.price <= +maxPrice;
     });
-    res.status(200).json(filteredArray);
-  } else if (prefix) {
-    const filteredArray: CartItem[] = dummyCartItems.filter((item) => {
+  }
+  if (prefix) {
+    temporaryCart = temporaryCart.filter((item) => {
       return item.product.startsWith(prefix as string);
     });
-    res.status(200).json(filteredArray);
-  } else if (pageSize) {
-    // const limitedArray: CartItem[] = [];
-    // for (let i = 0; i < +pageSize; i++){
-    //     limitedArray.push(dummyCartItems[i]);
-    // }
-    // res.status(200).json(limitedArray);
-    const limitedArray: CartItem[] = dummyCartItems.slice(0, +pageSize);
-    res.status(200).json(limitedArray);
-  } else {
-    res.status(200).json(dummyCartItems);
   }
+  if (pageSize) {
+    temporaryCart = temporaryCart.slice(0, +pageSize);
+  }
+  res.status(200).json(temporaryCart);
 });
 
 cartRouter.get("/cart-items/:id", (req, res) => {
-  const id: string = req.params.id;
-  const cartItem: CartItem | undefined = dummyCartItems.find((item) => {
-    return item.id === +id;
-  });
-  if (cartItem !== undefined) {
-    res.status(200).json(cartItem);
+  // get id from path param
+  const pathParamId: number = parseInt(req.params.id);
+  // look for item with that id
+  const itemThatImLookingFor: CartItem | undefined = dummyCartItems.find(
+    (item) => item.id === pathParamId
+  );
+  if (itemThatImLookingFor !== undefined) {
+    // we found that item by the path param
+    res.status(200).json(itemThatImLookingFor);
   } else {
-    res.status(404);
-    res.json({ message: "ID Not Found" });
+    // didn't find anything with that path param
+    res.status(404).json("ID Not Found");
   }
 });
 
@@ -52,34 +48,45 @@ cartRouter.post("/cart-items", (req, res) => {
   res.status(201).json(newCartItem);
 });
 
-cartRouter.put("/cart-items/:id", (req, res) => {
-  const idOfCartItemToReplace: number = +req.params.id;
+cartRouter.put("/cart-items/:zebraId", (req, res) => {
+  // req body tell us how to replace an obj
   const updatedCartItem: CartItem = req.body;
-  const foundIndex = dummyCartItems.findIndex((item) => {
-    return item.id === idOfCartItemToReplace;
-  });
+  // path param point to which thing to replace
+  const idOfCartItemToReplace: string = req.params.zebraId;
+  // use that path param id to find location of obj to replace
+  const foundIndex = dummyCartItems.findIndex(
+    (temp) => temp.id === +idOfCartItemToReplace
+  );
   if (foundIndex !== -1) {
+    // replace obj from found location with data from body
     dummyCartItems[foundIndex] = updatedCartItem;
     res.status(200).json(updatedCartItem);
   } else {
-    res.status(404).json({
-      message: "cart item not found with id: " + idOfCartItemToReplace,
-    });
+    res
+      .status(404)
+      .json({ message: "nothing found with id: " + idOfCartItemToReplace });
   }
 });
 
-cartRouter.delete("/cart-items/:id", (req, res) => {
-  const idOfCartItemToDelete: number = +req.params.id;
-  const foundIndex = dummyCartItems.findIndex((item) => {
-    return item.id === idOfCartItemToDelete;
+cartRouter.delete("/cart-items/:idToDelete", (req, res) => {
+  // grab id from path param
+  const idOfThingToDelete: number = +req.params.idToDelete;
+  // use that id to find location (index), so that we can delete it
+  const foundIndex: number = dummyCartItems.findIndex((object) => {
+    // return a number (index) based on this condition:
+    // one of our objs has an id property === path param as a number
+    return object.id === idOfThingToDelete;
   });
+  // if it finds that thing: (not -1)
   if (foundIndex !== -1) {
+    // we found it
+    // delete it
     dummyCartItems.splice(foundIndex, 1);
-    res.status(204);
+    // delete only http method dont send body - instead use sendStatus
+    res.sendStatus(204); // 204 - no content (successful, nothing to see)
   } else {
-    res.status(404).json({
-      message: "cart item not found with id: " + idOfCartItemToDelete,
-    });
+    // if we didn't find it, don't delete and send 404
+    res.status(404).json({ message: "ID not found" });
   }
 });
 
